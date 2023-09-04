1716383e11f20eb79ff3461edd43541d9a2904a0
Rennan Monteiro <rennan.monteiro@etec.sp.gov.br>
2023-09-04 10:13:49 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string Nome1, Nome2;
+
+Console.Write("Escreva seu Nome:");
+Nome1=Console.ReadLine();
+
+Console.Write("Escreva seu Nome:");
+Nome2=Console.ReadLine();
+
+Console.WriteLine("-------Troca de Valores -------");
+Console.WriteLine($"{Nome1}, {Nome2}");
+Console.WriteLine("-Após a Troca de Valores, Temos:");
+Console.WriteLine($"{Nome2}, {Nome1}");
